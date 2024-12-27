4674d1d945b126fba9d3a01fc3e17fae42b922a3
Pheet0123 <pheet0123@gmail.com>
2024-12-27 22:58:40 +0700

    100%

--- app.js
+++ app.js
@@ -1,24 +1,29 @@
 const celciusInput = document.getElementById("celcius")
+//สร้างตัวแปลเพื่อนำข้อมูลจากเอลสเมลนั้นๆมาเพื่อไปคำนวณ
 const fahrenheitInput = document.getElementById("fahrenheit")
 const kelvinInput = document.getElementById("kelvin")
 const tempInputs = document.querySelectorAll("input")
-
-tempInputs.forEach(input => {
-    input.addEventListener("input",(e)=>{
+//ตรวจสอบว่ามีการป้อนข้อมูลเข้ามารึป่าว โดยการตรวจไปเรื้อยๆ
+tempInputs.forEach(input => {  
+    input.addEventListener("input",(e)=>{   
+        //ตัวเลขที่ป้อนเข้ามา และให้เป็นจำนวนเต็ม
         let tempValue = parseInt(e.target.value)
-
+        //ชื่อหน่วยอุณหภูมิ ที่สนใจ
         let inputName = e.target.name
-        
+
+        //เมื่อยังไม่มีการใส่ข้ออะไรลงช่องจะปล่อยค่าว่าง
         if(e.target.value === ""){
             celciusInput.value = null
             fahrenheitInput.value = null
             kelvinInput.value = null
             return
         }
-
+        //ดูว่าชื่อตรงกับ ช่อง name ช่องไหน
         if(inputName === "celcius"){
+                            //นำตัวเลขที่รับไปคำนวณตามสูตร
             let fahrenheit = tempValue *1.8 +32
             fahrenheitInput.value = fahrenheit.toFixed(2)
+            //นำข้อความไปแสดงผ้านตัวนี้
 
             let kelvin = tempValue +273
             kelvinInput.value = kelvin.toFixed(2)
